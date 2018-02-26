644d92a908b93f9a83d0d5dfd45a1ffddb5aa16c
okirsanova <aspirjanda@gmail.com>
2018-02-26 21:16:15 +0200

    guess number "wanna play again? Yes/other"

--- src/TSI_Java_courses/Main.java
+++ src/TSI_Java_courses/Main.java
@@ -1,7 +1,5 @@
 package TSI_Java_courses;
 
-import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
-import jdk.internal.cmm.SystemResourcePressureImpl;
 
 import java.util.Random;
 import java.util.Scanner;
@@ -9,51 +7,48 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+
+
         Random random = new Random();
         Scanner scanner = new Scanner(System.in);
+        String answer;
+        do {
+            System.out.println("I'm thinking of a number from 1 to 100. Try to guess it!");
 
+            int randNum = random.nextInt(100) + 1;
+            System.out.println("Number is " + randNum);
 
-        do
-            System.out.println("Wanna play?");
-            String answer2 = scanner.next();
-
-            while (answer2.equals("no")) {
-                System.out.println("Ok. Good bye.");
-            }
-
-            while (answer2.equals("")) {
-                System.out.println("Ok... Good bye.");
-            }
-
-            while (answer2.equals("yes")) {
-                System.out.println("I'm thinking of a number from 1 to 100. Try tp guess it!"); }
-                {
-
-                    int randNum = random.nextInt(100) + 1;
-
-                    System.out.println(randNum);
-                }
+            for (int i = 0; i < 5; i++) {
+                System.out.println("Try # " + (i + 1));
 
                 int userNum = scanner.nextInt();
-                int randNum = random.nextInt(100) + 1;
 
-                for (int i = 0; i < 5; i++) {
-                    System.out.println("Try # " + (i + 1));
-                    {
-                        System.out.println(userNum);
-                    }
+                System.out.println(userNum);
 
-                    if ((randNum) < (userNum)) {
+                if ((randNum) < (userNum)) {
 
-                        System.out.println("My Number is Higher");
-                    } else if ((randNum) > (userNum)) {
+                    System.out.println("My Number is Higher");
+                } else if ((randNum) > (userNum)) {
 
-                        System.out.println("My Number is Lower");
+                    System.out.println("My Number is Lower");
+                } else if ((randNum) == (userNum)) {
 
-                    } else if ((randNum) == (userNum)) {
+                    System.out.println("That is My Number!!!");
+                    break;
+                }
 
-                        System.out.println("That is My Number!!!");
-                        break;
+            }
+            System.out.println("Once more?");
+            answer = scanner.next();
+            System.out.println(answer);
+
+        } while (answer.equals("yes"));
+
+        System.out.println("Good bye!");
+
+    }
+}
+
 
 
 
