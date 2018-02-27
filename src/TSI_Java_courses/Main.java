d42830ec1b2c230d726d989e30bd52abb6222468
okirsanova <aspirjanda@gmail.com>
2018-02-27 20:38:01 +0200

    guess number "yes/no and error exceprion for wrong input"

--- src/TSI_Java_courses/Main.java
+++ src/TSI_Java_courses/Main.java
@@ -1,16 +1,18 @@
 package TSI_Java_courses;
 
 
+import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
 
 
         Random random = new Random();
-        Scanner scanner = new Scanner(System.in);
         String answer;
         do {
             System.out.println("I'm thinking of a number from 1 to 100. Try to guess it!");
@@ -19,27 +21,29 @@ public class Main {
             System.out.println("Secret Number is " + randNum);
 
             for (int i = 1; i <= 10; i++) {
-                System.out.println("Try # " + i );
+                System.out.println("Try # " + i);
 
-                int userNum = scanner.nextInt();
+                int userNum = askNumber();
+
                 System.out.println(userNum);
 
                 if (randNum < userNum) {
 
-                    System.out.println("My Number is Higher");}
+                    System.out.println("My Number is Lower");
+                } else if (randNum > userNum) {
 
-                else if (randNum > userNum) {
-
-                    System.out.println("My Number is Lower"); }
+                    System.out.println("My Number is Higher");
+                }
 
                 if (randNum == userNum) {
 
                     System.out.println("That is My Number!!!");
-                    break; }
+                    break;
+                }
 
                 if (i == 10) {
-                    System.out.println("Looser! My number was " + userNum); }
-
+                    System.out.println("Looser! My number was " + randNum);
+                }
             }
 
             System.out.println("Once more?");
@@ -51,7 +55,41 @@ public class Main {
         System.out.println("Good bye!");
 
     }
+
+    static String askAnswer() {
+        for (; ; ) {
+            try {
+                String askAnswer = scanner.next();
+                if (askAnswer.equalsIgnoreCase("YES")) {
+                    System.out.println("Try again!");
+                }
+                if (askAnswer.equalsIgnoreCase("NO")) {
+                    System.out.println("Bye");
+                }
+            } catch (InputMismatchException e) {
+                scanner.next();
+                System.out.println("Type an answer YES or NO!");
+            }
+        }
+    }
+
+
+    static int askNumber() {
+        for (; ; ) {
+            try {
+                int num = scanner.nextInt();
+                if (num <= 100 && num >= 1) {
+                    return num;
+                }
+                System.out.println("Oh no! Number " + num + " is in wrong range. Try between 1 and 100");
+            } catch (InputMismatchException e) {
+                scanner.next();
+                System.out.println("Only numbers, please!");
+            }
+        }
+    }
 }
+
 
 
 
